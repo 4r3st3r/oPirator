04f52d1016940b02e12adcfbe0115f7f7d9c1920
Hector Forrester <hector@forrester.org.uk>
2021-10-26 14:19:27 +0100

    changed test file

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ import time
 import RPi.GPIO as GPIO
 
 # GPIO pins
-BUTTON = 23
-LED = 25
-SPEAKER_RELAY = 22
-MIC_RELAY = 21
+BUTTON = 3
+LED = 5
+SPEAKER_RELAY = 7
+MIC_RELAY = 11
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
@@ -19,35 +19,23 @@ GPIO.setup(BUTTON, GPIO.IN)
 GPIO.setup(SPEAKER_RELAY, GPIO.OUT)
 GPIO.setup(MIC_RELAY, GPIO.OUT)
 
-GPIO.add_event_detect(BUTTON, GPIO.FALLING, bouncetime=500)
-GPIO.add_event_detect(BUTTON, GPIO.RISING, bouncetime=500)
+GPIO.add_event_detect(BUTTON, GPIO.BOTH, bouncetime=500)
 
 
 def handle_button():
-    press_threshold = 2
-
     if GPIO.event_detected(BUTTON):
         print("GPIO.event_detected")
-        pressed_time = time.time()
 
-        if GPIO.input(BUTTON, GPIO.RISING):  # If button is being pressed
-            pressed_time = time.time() - pressed_time  # Calculate the time it was pressed for
+        if GPIO.input(BUTTON) is False:  # If button is being pressed
+            print("---- PHONE ON HOOK ----")
+            print("mycroft.stop")  # Stop all actions
+            GPIO.output(MIC_RELAY, GPIO.LOW)  # Deactivate mic
+            print('MIC OFF')
+            time.sleep(2)
+            GPIO.output(SPEAKER_RELAY, GPIO.HIGH)  # Activate loudspeaker
+            print('SPEAKER ON')
 
-            if pressed_time < press_threshold:  # If short press:
-                print("---- RESET BUTTON PRESSED ----")
-                print("mycroft.stop")  # Stop current action
-                time.sleep(0.2)  # Wait
-                print("mycroft.mic.listen")  # Start listening again for new command
-
-            else:  # If long press
-                print("---- PHONE ON HOOK ----")
-                print("mycroft.stop")  # Stop all actions
-                GPIO.output(MIC_RELAY, GPIO.LOW)  # Deactivate mic
-                print('MIC OFF')
-                GPIO.output(SPEAKER_RELAY, GPIO.HIGH)  # Activate loudspeaker
-                print('SPEAKER ON')
-
-        elif GPIO.input(BUTTON, GPIO.FALLING):  # If button is now not being pressed
+        elif GPIO.input(BUTTON) is True:  # If button is now not being pressed
             print("---- PHONE OFF HOOK ----")
             print("mycroft.mic.listen")  # Start listening
             GPIO.output(MIC_RELAY, GPIO.HIGH)  # Activate Microphone
